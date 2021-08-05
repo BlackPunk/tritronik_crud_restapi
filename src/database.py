4ed66ca02ce04d950e8e220b6be403a2e9b97918
BlackPunk <blackpunk1337@gmail.com>
2021-08-05 16:16:30 +0700

    inisialisasi

--- src/database.py
+++ src/database.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine("mysql+pymysql://root@localhost:3306/tritronik_3", echo=True)
+engine = create_engine(
+    "postgres+psycopg2://delokbwrpzirgy:08feaf754542d4ce0a7a553475b6abfc431909084891ed328502de1112e8f2ef@ec2-54-196-65-186.compute-1.amazonaws.com:5432/damigk5alv2cbj", echo=True)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
